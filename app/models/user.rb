a83b2b2f8bd6fd0b0eac378fbb682b32aa61ed6d
kitavi <kitavimjoseph@gmail.com>
2016-05-06 22:47:24 +0000

    image changes 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>" }, default_url: "/images/:style/missing.png"
+         has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>" }
         validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
