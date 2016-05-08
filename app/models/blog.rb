816948a4255eebcb105bdc27de667eaa292d50f8
kitavi <kitavimjoseph@gmail.com>
2016-05-08 17:36:45 +0000

    image tag updates

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,4 @@
 class Blog < ActiveRecord::Base
      has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
      validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
