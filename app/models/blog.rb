10172d8bdca5a5a3d95892dedcc77b36175fb027
kitavi <kitavimjoseph@gmail.com>
2016-05-08 21:27:57 +0000

    m55

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ActiveRecord::Base
-     has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }, default_url: "missing.png"
+     has_attached_file :image, styles: { large: "1000x600>", medium: "300x300>", thumb: "150x150#" }, default_url: "missing.png"
      validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
      
      validates :title, presence: true,
